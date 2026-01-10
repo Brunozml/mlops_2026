d8335e8f46daa41f91000d7ae01bc4e6418f7e18
Brunozml <bzorrilla02@gmail.com>
2026-01-10 12:07:06 +0100

    s4 fixed shape bug. 3 more to go

---
+++ s4_debugging/agent_test/src/agent_test/train.py
@@ -0,0 +1,10 @@
+from agent_test.model import Model
+from agent_test.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
---
+++ s4_debugging/readme.md
@@ -0,0 +1,7 @@
+# Debugging, Profiling, and Logging
+
+@ Thursday January 8th, 2026
+missed due to in-person work. 
+
+TODO @Sat Jan 9th
+
